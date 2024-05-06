7e46f31c03ca30b4cbbadacefe9ca47b306de82c
Eduardo Castillo Hexaware <EduardoC@hexaware.com>
2024-05-06 08:48:07 -0600

    Add Index In Tables

--- SQLUpgrade.sql
+++ SQLUpgrade.sql
@@ -4,27 +4,31 @@ GO
 USE users
 GO
 
-	CREATE TABLE dbo.roles(
-		id int PRIMARY KEY IDENTITY(1,1),
-		rolev varchar(50) 
-	)
-	GO
+CREATE TABLE dbo.roles(
+	id int PRIMARY KEY IDENTITY(1,1),
+	rolev varchar(50) 
+)
+GO
 
-	INSERT dbo.roles(rolev) VALUES('admin') 
-	GO
-	INSERT dbo.roles(rolev) VALUES('operator') 
-	GO
-	GO
+INSERT dbo.roles(rolev) VALUES('admin') 
+GO
+INSERT dbo.roles(rolev) VALUES('operator') 
+GO
 
 
-	CREATE TABLE users(
-		id int PRIMARY KEY IDENTITY(1,1),
-		userName varchar(50) ,
-		[password] varchar(50) ,
-		roleId int ,
-		CONSTRAINT fk_roleId FOREIGN KEY (roleId) REFERENCES roles(id)
-	)
-	GO
+CREATE TABLE users(
+	id int PRIMARY KEY IDENTITY(1,1),
+	userName varchar(50) ,
+	[password] varchar(50) ,
+	roleId int ,
+	CONSTRAINT fk_roleId FOREIGN KEY (roleId) REFERENCES roles(id)
+)
+GO
 
-	INSERT dbo.users (userName,[password],roleId) VALUES('admin','admin',1) 
-	GO
+INSERT dbo.users (userName,[password],roleId) VALUES('admin','admin',1) 
+GO
+
+CREATE UNIQUE INDEX roleIndex ON dbo.roles (id)
+GO
+CREATE UNIQUE INDEX userIndex ON dbo.users (id)
+GO
